8b92aaf8d9b6d1b38cb20ec76925bc16c923f7e0
siri1577 <54629585+siri1577@users.noreply.github.com>
2019-08-28 09:15:10 -0400

    Add files via upload

--- exerciseObjectivesRunner.java
+++ exerciseObjectivesRunner.java
@@ -5,10 +5,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		plugin = {"progress",
-	            "html:build/report/html",
-	            "junit:build/report/junit/cucumber-report.xml",
-	            "json:build/report/json/cucumber-report.json"
-	    },
+	            "html:report/junit/cucumber-html-report",
+	             },
         features = { "src/com/Exercise/Testcases/exerciseObjectives.feature"},
                 glue={"src.com.Exercise.Testcases"}
 
